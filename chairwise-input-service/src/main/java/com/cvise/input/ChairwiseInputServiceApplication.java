4c19930a0293166acb00589f2ad71d14de551883
sidd-satish <sidd.satish95@gmail.com>
2019-10-06 23:09:09 +0800

    Added Kubernetes Config

--- chairwise-input-service/src/main/java/com/cvise/input/ChairwiseInputServiceApplication.java
+++ chairwise-input-service/src/main/java/com/cvise/input/ChairwiseInputServiceApplication.java
@@ -3,10 +3,12 @@ package com.cvise.input;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class ChairwiseInputServiceApplication {
 
 	public static void main(String[] args) {
